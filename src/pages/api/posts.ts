67f40c29a4e704c16d62f53f8a3345359d7017e5
senspond20 <senspond@gmail.com>
2021-05-13 03:05:00 +0900

    pp

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -7,7 +7,7 @@ export default (req : NextApiRequest, res : NextApiResponse) => {
         const data = fs.readFileSync(file, 'utf-8');
         res.status(200).json(JSON.parse(data));
     }catch (e) {
-        res.status(500).json({code : 500, message : '서버장애 - 데이터 조회에 실패했당께', date : new Date()})
+        res.status(500).json({code : 500, message : '데이터 조회에 실패했당께', date : new Date()})
     }
 
     // res.writeHead(200, {'Content-Type': 'application/json; charset=utf8'});
