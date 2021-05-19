8fcc547e1943ac2b5373c105177bab5182b56dd2
senspond20 <senspond@gmail.com>
2021-05-19 21:39:17 +0900

    fix

--- src/components/Templates/styles/Theme/index.ts
+++ src/components/Templates/styles/Theme/index.ts
@@ -1,14 +1,14 @@
 import {lightTheme} from './LightTheme'
 import {darkTheme} from './DarkTheme'
-import useThemeSwiter from './UseThemeSwiter'
+
 const ThemeType = {
     dark : 'dark',
     light : 'light',
     default : 'light'
-  }
+}
+
 export {
     ThemeType,
-    useThemeSwiter,
     darkTheme,
     lightTheme,
 }
