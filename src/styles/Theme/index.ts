15dc5506fd252d5898766192649914e324936cd6
senspond20 <senspond@gmail.com>
2021-05-19 18:47:02 +0900

    theme

---
+++ src/styles/Theme/index.ts
@@ -0,0 +1,13 @@
+import {lightTheme} from './LightTheme'
+import {darkTheme} from './DarkTheme'
+
+const ThemeType = {
+    dark : 'dark',
+    light : 'light',
+    default : 'light'
+}
+export {
+    ThemeType,
+    darkTheme,
+    lightTheme,
+}
